3dc319e67535ad2774ac55f0c6f78b61c99f5dbb
Abhinaba Basu <abhinaba@live.com>
2022-11-20 18:37:09 -0800

    Fixed deploy script to ensure right container is stopped before new deployment

--- service/deploy.sh
+++ service/deploy.sh
@@ -3,6 +3,6 @@
 # curl -fsSL get.docker.com -o get-docker.sh
 # sh get-docker.sh
 
-docker ps --filter="ancestor=wordament:0.1" -q | xargs docker stop
+docker ps --filter="ancestor=bonggeek/wordament" -q | xargs docker stop
 docker pull bonggeek/wordament
 docker run -d --restart="always" -p 8090:8090 bonggeek/wordament
